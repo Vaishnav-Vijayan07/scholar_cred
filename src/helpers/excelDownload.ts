233c1b5eab333dccb540bcfa6bfbb5279ba2c41c
Vaishnav Vijayan <83777916+Vaishnavtdy@users.noreply.github.com>
2024-03-08 16:59:19 +0530

    changes

--- src/helpers/excelDownload.ts
+++ src/helpers/excelDownload.ts
@@ -58,8 +58,8 @@ const excelDownload = (data: any, colom: any) => {
     worksheet.addRow(values);
   });
 
-  worksheet.eachRow(function (row, rowNumber) {
-    row.eachCell(function (cell, colNumber) {
+  worksheet.eachRow(function (row: any, rowNumber: any) {
+    row.eachCell(function (cell: any, colNumber: any) {
       const cellValue = cell.value ? cell.value.toString() : "";
       const cellLength = cellValue.length;
       const column = worksheet.getColumn(colNumber);
@@ -69,7 +69,7 @@ const excelDownload = (data: any, colom: any) => {
     });
   });
 
-  workbook.xlsx.writeBuffer().then((buffer) => {
+  workbook.xlsx.writeBuffer().then((buffer: any) => {
     saveAsExcelFile(buffer, "data.xlsx");
   });
 };
